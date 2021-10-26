f6c5c243957b75540c51daea62721905391d2070
ScottDonovan <spdonovan@hotmail.co.uk>
2021-10-26 18:04:02 +0100

    Get and Post methods added to app

--- app.rb
+++ app.rb
@@ -13,7 +13,17 @@ get '/page_1' do
   "Another empty page"
 end
 
-get '/cat' do
-erb(:index)
+get '/random-cat' do
+@name = ["Amigo", "Misty", "Almond"].sample
+erb :index
 end
 
+post '/named-cat' do
+  p params
+@name = params[:name]
+erb :index
+end
+
+get '/cat-form' do
+  erb :cat_form
+end
